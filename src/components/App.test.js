196c9fb4203038eb706b0f8361dee91a95db2113
Sandra Naim <sandra.naim@toothpickapp.com>
2022-01-17 13:48:32 +0200

    apply more behavior driven development (BDD) to reduce bugs

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,23 +2,34 @@ import React from "react";
 import { shallow } from 'enzyme';
 import App from './App';
 
-const app = shallow(<App />);
+describe('App', () => {
+  
+  const app = shallow(<App />);
 
+  it('renders correctly', () => {
+    expect(app).toMatchSnapshot();
+  })
 
-it('renders correctly', () => {
-  expect(app).toMatchSnapshot();
-})
+  it('initialize a state of an empty gifts array', () => {
+    expect(app.state().gifts).toEqual([]);
+  })
 
-it('initialize a state of an empty gifts array', () => {
-  expect(app.state().gifts).toEqual([]);
-})
+  describe('after clicking the `add-btn`', () => {
+    beforeEach(() => {
+      app.find("[data-test='add-btn']").simulate('click');
+    })
 
-it('add new gift after clicking on the button', () => {
-  app.find("[data-test='add-btn']").simulate('click');
-  expect(app.state().gifts).toEqual([{id: 1}])
-})
+    afterEach(() => {
+      app.setState({gifts: []})
+    })
 
-it('add a new gift to the gifts list after clicking the add button', () => {
-  app.find("[data-test='add-btn']").simulate('click');
-  expect(app.find("[data-test='gift-list']").children().length).toEqual(2);
+    it('add new gift state', () => {
+      expect(app.state().gifts).toEqual([{id: 1}])
+    })
+  
+    it('add a new gift to the gifts list', () => {
+      expect(app.find("[data-test='gift-list']").children().length).toEqual(1);
+    })
+  })
+ 
 })
